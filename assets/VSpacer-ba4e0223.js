1b1b616c0261d5f37d8ff38c1ac2a886d87706db
GregoryKogan <grisha.koganovskiy@gmail.com>
2023-07-07 14:46:44 +0300

    deploy

--- assets/VSpacer-ba4e0223.js
+++
@@ -1,1 +0,0 @@
-import{f as e}from"./VCard-f0c7a4ba.js";const a=e("flex-grow-1","div","VSpacer");export{a as V};
---
+++ assets/VSpacer-c78e7d1a.js
@@ -0,0 +1,1 @@
+import{f as e}from"./VCard-813b300f.js";const a=e("flex-grow-1","div","VSpacer");export{a as V};
